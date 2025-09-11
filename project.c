02ca56e317b60cd66852403438dbfbd3d3674716
Oughlane <oughlanemohammed72@gmail.com>
2025-09-11 02:27:24 +0100

    5functions with errors but alot of errors

--- project.c
+++ project.c
@@ -1,86 +1,140 @@
 #include <stdio.h>
-#include<string.h>
-struct Joueur equipe[MAX];
-int n = 0;         
-int nextId = 1;     
+#include <string.h>
 
-void ajouterUnJoueur() {
-    struct Joueur j;
-    j.id = nextId++;  
 
-    printf("Nom : ");
-    scanf("%s", j.nom);
-    printf("Prenom : ");
-    scanf("%s", j.prenom);
-    printf("Age : ");
-    scanf("%d", &j.age);
+int idAeroport = 1;
+int idAvion = 1;
+int nbrAvion = 0;
 
-    equipe[n++] = j;  
+typedef struct {
+    int jour;
+    int mois;
+    int annee;
+} Date;
 
-    printf("Joueur %s %s ajouté avec ID = %d\n", j.nom, j.prenom, j.id);
-}
- 
-int j_max;
-struct joueur
-{
+typedef struct {
     int id;
-    char full_name[50];
-    int num_maillot;
-    int age;
-    int buts;
-    char poste[4];
-    char statut[2];
-};
+    char modele[20];
+    int capacite;
+    char statut[20]; 
+    Date dateEntree;
+} Avion;
 
-void ajouterUnJoueur();
-void ajouterPlusieurs();
-void afficher_ordre();
-void afficherParNom();
-void afficherParAge();
-void afficherParPoste();
-void modifierJoueur();
-void supprimerJoueur();
-void chercherParId();
-void chercherParNom();
-void statistiques();
+typedef struct {
+    int id;
+    char nom[50];
+    Avion flotte[40];
+} Aeroport;
 
+void errMessage(char message[]) {
+    printf("%s", message);
+} 
+void afficherAeroport(Aeroport a) {
+    printf("Id Aeroport : %d | Nom : %s \n", a.id, a.nom);
+    printf("------------- Flotte d'avions ---------------\n");
+    for (int i = 0; i < nbrAvion; i++) {
+        printf("Avion %d : ======================> \n", i+1);
+        printf("Id : %d | Modele : %s | Capacite : %d | Statut : %s | Date entree : %d/%d/%d \n",
+               a.flotte[i].id, a.flotte[i].modele, a.flotte[i].capacite, a.flotte[i].statut,
+               a.flotte[i].dateEntree.jour, a.flotte[i].dateEntree.mois, a.flotte[i].dateEntree.annee);
+    }
+}
+
+Avion ajouterAvion(Aeroport a) {
+    Avion av;
+    av.id = idAvion++;
+    printf("Modele : ");
+    scanf("%s", av.modele);
+    printf("Capacite : ");
+    scanf("%d", &av.capacite);
+    printf("Statut (Disponible / En_vol / Maintenance) : ");
+    scanf("%s", av.statut);
+    printf("Date entree (jour mois annee) : ");
+    scanf("%d %d %d", &av.dateEntree.jour, &av.dateEntree.mois, &av.dateEntree.annee);
+
+    return av;
+}
+
+Aeroport ajouterAvionAeroport(Aeroport a) {
+    afficherAeroport(a);
+    a.flotte[nbrAvion] = ajouterAvion(a);
+    nbrAvion++;
+    return a;
+}
+
+
+Avion RechercheById(int id, Aeroport a) {
+    int index = -1;
+    for (int i = 0; i < nbrAvion; i++) {
+        if (a.flotte[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1) {
+        printf("Avion trouv \n");
+        return a.flotte[index];
+    } else {
+        printf("Avion introuvable\n");
+        Avion vide;
+        vide.id = -1;
+        return vide;
+    }
+}
+
+// ------------------ SUPPRESSION ------------------
+Aeroport supprimerAvion(int id, Aeroport a) {
+    int index = -1;
+    for (int i = 0; i < nbrAvion; i++) {
+        if (a.flotte[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1) {
+        for (int i = index; i < nbrAvion - 1; i++) {
+            a.flotte[i] = a.flotte[i + 1];
+        }
+        Avion avVide;
+        a.flotte[nbrAvion - 1] = avVide;
+        nbrAvion--;
+        printf("Avion supprimé avec succès ✈️\n");
+    } else {
+        printf("Suppression impossible : avion introuvable ❌\n");
+    }
+    return a;
+}
+
+// ------------------ MAIN ------------------
 int main() {
-    int choix;
+    int choix = 0;
+    Aeroport aeroport;
 
     do {
-        printf("\n===== MENU GESTION EQUIPE DE FOOT =====\n");
-        printf("1. Ajouter un joueur\n");
-        printf("2. Ajouter plusieurs joueurs\n");
-        printf("3. Afficher tous les joueurs\n");
-        printf("4. Afficher joueurs par nom (\n");
-        printf("5. Afficher joueurs par age \n");
-        printf("6. Afficher joueurs par poste\n");
-        printf("7. Modifier un joueur\n");
-        printf("8. Supprimer un joueur\n");
-        printf("9. Rechercher un joueur par ID\n");
-        printf("10. Rechercher un joueur par nom\n");
-        printf("11. Statistiques\n");
-        printf("12. Quitter\n");
+        printf("\n========== MENU ==========\n");
+        printf("1 - Ajouter Aeroport\n");
+        printf("2 - Ajouter Avion\n");
+        printf("3 - Afficher Aeroport\n");
+        printf("4 - Rechercher un Avion par ID\n");
+        printf("5 - Supprimer un Avion\n");
+        printf("100 - Quitter\n");
         printf("Votre choix : ");
         scanf("%d", &choix);
 
         switch (choix) {
-            case 1: ajouterUnJoueur(); break;
-            case 2: ajouterPlusieurs(); break;
-            case 3: afficherTous(); break;
-            case 4: afficherParNom(); break;
-            case 5: afficherParAge(); break;
-            case 6: afficherParPoste(); break;
-            case 7: modifierJoueur(); break;
-            case 8: supprimerJoueur(); break;
-            case 9: chercherParId(); break;
-            case 10: chercherParNom(); break;
-            case 11: statistiques(); break;
-            case 12: printf("Au revoir !\n"); break;
-            default: printf("Choix invalide, reessayez.\n");
+            case 1:
+                aeroport.id = idAeroport++;
+                printf("Nom de l'aeroport : ");
+                scanf("%s", aeroport.nom);
+                break;
+            case 2:
+                aeroport = ajouterAvionAeroport(aeroport);
+                break;
+            case 3:
+                afficherAeroport(aeroport);
+                break;
+            case 4: 
+                afficherAeroport(aeroport);
+                break;
         }
-
-    } while (choix != 12);
-
-    return 0;
-}
